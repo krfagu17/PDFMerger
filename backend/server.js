97c070249558df0e7a5bda8dec98faffc40a449d
krfagu17 <krfagu17@gmail.com>
2024-01-09 15:13:13 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,81 @@
+import express from 'express';
+import multer from 'multer';
+import cors from 'cors';
+import path from 'path';
+import PDFMerger from 'pdf-merger-js';
+import fs from 'fs';
+
+const app = express();
+
+const merger = new PDFMerger();
+
+// Enable CORS
+app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname);
+        cb(null, file.originalname + '-' + Date.now() + ext);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
+let lastMergedFilePath ;
+// Define a route for file uploads and merging
+app.post('/', upload.array('files'), async (req, res) => {
+    // Get the paths of the uploaded files
+    const filePaths = req.files.map(file => file.path);
+
+    // Get the original names of the uploaded files
+    const originalNames = req.files.map(file => path.basename(file.originalname, path.extname(file.originalname))).join('-'); 
+
+    const mergedFileName = originalNames + '-' + Date.now() + '.pdf';
+    // Merge the PDF files
+    lastMergedFilePath = `uploads/${mergedFileName}`;
+    try {
+        for (const path of filePaths) {
+            await merger.add(path);
+        }
+        await merger.save(lastMergedFilePath); //save under given name and reset the internal document
+        const absolutePath = path.resolve(lastMergedFilePath);
+        return res.json({ message: 'Files merged successfully', path: absolutePath });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error merging files' });
+    }
+});
+
+app.get('/download', (req, res) => {
+    const file = lastMergedFilePath;
+   if (!file) {
+        res.status(400).json({ message: 'No file to download' });
+    }   
+    res.download(file,(error)=>{
+        if(error){
+            res.status(500).json({ message: 'Error downloading file' });
+        }else{
+            //delete file after download
+            fs.unlink(file, (err) => {
+                if (err) {
+                    console.error(err)
+                    return
+                }else{
+                    console.log('file deleted successfully');}
+            })
+
+        }
+        
+    
+    }); // Set disposition and send it.
+});
+
+// Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [filesData, setFilesData] = useState("")
+  const [pdfData, setpdfData] = useState(null)
+  const [mergeStatus, setMergeStatus] = useState(null)
+
+  
+  
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    const formData = new FormData()
+    for(let i=0; i<filesData.length; i++){
+      formData.append('files', filesData[i])
+    }
+    // console.log(formData.getAll('files'))
+
+    try {
+      const response= await axios.post('http://localhost:5000/', formData)
+      setpdfData(response.data)
+      setMergeStatus(response.data.message)
+      
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+// const handleDownload = async(e) => {
+//   e.preventDefault()
+//   try {
+//     const response = await axios.get('http://localhost:5000/download')
+//     console.log(response)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+console.log(pdfData)
+  return (
+    <>
+      <div>
+      <h1>Upload Files</h1>
+        <form className='mainForm' action="submit" onSubmit={handleSubmit}>
+
+        <div className="container">
+
+        <input type='file' onChange={(e)=>setFilesData(e.target.files) } multiple />
+        {/* <input type='file' onChange={(e)=>setFilesData1(e.target.files[0]) } /> */}
+        <button type='submit'>Submit</button>
+        </div>
+        <span>{mergeStatus&&mergeStatus}</span>
+
+        <a href="http://localhost:5000/download">Download File</a>
+        </form>
+        </div>
+    </>
+  )
+}
+
+export default App
